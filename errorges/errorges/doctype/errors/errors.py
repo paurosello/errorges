4007c2ad377530746fdc4b42cea211f8ccb46530
Ubuntu <frappe@FrappeDev.0cb25syyhttubhrmwxmdwd2rpd.ax.internal.cloudapp.net>
2017-02-09 19:35:20 +0000

    only send mail if accoutn configure

--- errorges/errorges/doctype/errors/errors.py
+++ errorges/errorges/doctype/errors/errors.py
@@ -5,7 +5,10 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.email.smtp import SMTPServer, get_outgoing_email_account
 
 class Errors(Document):
 	def on_update(self):
-            frappe.sendmail(recipients="paurosello@gmail.com, santi@servipro.eu", subject="Error "+ self.titul +" Actualitzat: " + self.estat, message="http://13.80.115.15/desk#Form/Errors/"+self.name+"\n\n\n\n" + self.descripcio, sender="paur@servipro.eu")
+            email_account = get_outgoing_email_account(True)
+            if email_account.enable_outgoing == 1:
+                frappe.sendmail(recipients="paurosello@gmail.com, santi@servipro.eu", subject="Error "+ self.titul +" Actualitzat: " + self.estat, message="http://13.80.115.15/desk#Form/Errors/"+self.name+"\n\n\n\n" + self.descripcio, sender="paur@servipro.eu")
