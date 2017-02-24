21b41c32633c17b1ed5fd355cfb519b1de279874
paurosello <paurosello@gmail.com>
2017-02-24 10:57:13 +0100

    Create errorges.py

---
+++ errorges/config/errorges.py
@@ -0,0 +1,12 @@
+def get_data():
+    return [
+        {
+            "label": _("Errors"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "label": _("Errors"),
+                    "name": "Errors"
+                }
+        }
+    ]
